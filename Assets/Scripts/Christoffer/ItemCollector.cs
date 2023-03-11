67947ce8e4507322dba0aed574c4cbb4ffac4bdb
ChrisCPH <christoffer221199@hotmail.com>
2023-03-11 15:12:44 +0100

    Added UI elements

--- Assets/Scripts/Christoffer/ItemCollector.cs
+++ Assets/Scripts/Christoffer/ItemCollector.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-   private void OnTriggerEnter(Collider other) 
-   {
+    int coins = 0;
+
+    [SerializeField] TextMeshProUGUI coinsText;
+
+    private void OnTriggerEnter(Collider other) 
+    {
         if (other.gameObject.CompareTag("Coin")) 
         {
             Destroy(other.gameObject);
+            coins++;
+            coinsText.text = "Coins collected: " + coins;
         }
    }
 }
