a697ee48d288888c2b2e2e76f3595035b819490c
Onii-chan <failer007@live.dk>
2023-04-24 14:01:37 +0200

    Added thirdperson camera + initial treasure scene

--- Assets/Scripts/Carlo/MovementScriptCD.cs
+++ Assets/Scripts/Carlo/MovementScriptCD.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Carlo;
 using UnityEngine;
 
 /*
@@ -11,11 +12,15 @@ public class MovementScriptCD : MonoBehaviour
     public bool AllowJump = true;
     public float JumpStrength = 5f;
     public float MovementSpeed = 10f;
-    public float RotationSpeed = 75f;
+    public float RotationSpeed = 10f;
     public float AnimationDampTime = .1f;
-    public float gravityScale = 1.0f;
-    public static float globalGravity = -9.81f; // Unity's default gravity value. See Project Settings -> Physics -> Gravity.
+    public float GravityScale = 1.0f;
+    public static string MainCameraTag = "MainCamera";
+    public static float GlobalGravity = -9.81f; // Unity's default gravity value. See Project Settings -> Physics -> Gravity.
+
+    [Header("Player Components")]
     public Rigidbody Rigidbody;
+    public ThirdPersonCameraCD Camera;
 
     Vector3 movementVector;
     Vector3 gravityVector;
@@ -35,18 +40,21 @@ public class MovementScriptCD : MonoBehaviour
             Rigidbody.freezeRotation = true;
             Rigidbody.useGravity = false;
         }
+
+        Camera = GameObject.FindGameObjectWithTag(MainCameraTag).GetComponent<ThirdPersonCameraCD>();
     }
 
     private void Update()
     {
         HandleAnimation();
-        HandleMovement();
+        HandleInput();
         HandleJump();
     }
 
     private void FixedUpdate()
     {
         // Physics based stuff needs to be in FixedUpdate().
+        HandleMovement();
         HandleGravity();
     }
 
@@ -59,7 +67,7 @@ public class MovementScriptCD : MonoBehaviour
         }
     }
 
-    void HandleMovement()
+    void HandleInput()
     {
         // Y
         float horizontal = Input.GetAxis("Horizontal");
@@ -67,12 +75,21 @@ public class MovementScriptCD : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         movementVector = new Vector3(horizontal, 0, vertical) * MovementSpeed;
-        movementVector = transform.TransformDirection(movementVector);
+        //movementVector = transform.TransformDirection(movementVector);
+
+        // limit the input
+        if (movementVector.magnitude > 1f)
+            movementVector.Normalize();
+    }
+
+    void HandleMovement()
+    {
 
         Rigidbody.velocity = movementVector;
-        transform.Rotate(0, horizontal * RotationSpeed, 0);
+        Rotate(movementVector, RotationSpeed);
+        //RotateToDirection(Camera.transform.forward, RotationSpeed);
 
-        // TODO: Fix forward + backward inputs reversed when moving.
+        // TODO: Fix rotation orientation for movement.
     }
 
     void HandleJump()
@@ -86,7 +103,30 @@ public class MovementScriptCD : MonoBehaviour
         // Handle gravity manually. We will add constant force of 
         // the global gravity along with the gravity scale to simulate it.
         // Unity's gravity value can be found under Project Settings -> Physics -> Gravity.
-        gravityVector = globalGravity * gravityScale * Vector3.up;
+        gravityVector = GlobalGravity * GravityScale * Vector3.up;
         Rigidbody.AddForce(gravityVector, ForceMode.Acceleration);
+    }
+
+    public virtual void RotateToDirection(Vector3 direction, float rotationSpeed)
+    {
+        direction.y = 0f;
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, direction.normalized, rotationSpeed * Time.deltaTime, .1f);
+        Quaternion _newRotation = Quaternion.LookRotation(desiredForward);
+        transform.rotation = _newRotation;
+    }
+
+    public void Rotate(Vector3 direction, float angularSpeed, bool onlyLateral = true)
+    {
+        if (onlyLateral)
+            direction = Vector3.ProjectOnPlane(direction, transform.up);
+
+        if (direction.sqrMagnitude < 0.0001f)
+            return;
+
+        var targetRotation = Quaternion.LookRotation(direction, transform.up);
+        var newRotation = Quaternion.Slerp(Rigidbody.rotation, targetRotation,
+            angularSpeed * Time.deltaTime);
+
+        Rigidbody.MoveRotation(newRotation);
     }
 }
